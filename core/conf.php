bd4dd62ad6d63977879b1aee2c26a6c184f277bf
nils <nils@debian.(none)>
2009-04-16 11:44:04 -0700

    merge from stage to master

--- core/conf.php
+++ core/conf.php
@@ -1,9 +1,9 @@
 <?php
 
 $loc = '';
-$path = '/var/vhosts/dropbox-stage.easytospell.net';
-$version = '0.1.0a';
-$url = 'dropbox-stage.easytospell.net';
+$path = '/var/vhosts/dropbox.easytospell.net';
+$version = '0.2.0a';
+$url = 'dropbox.easytospell.net';
 $secure = true; // use https for things that require passwords, upload, edit, delete
 
 $db = new mysqli("localhost", "dropbox", "dropbox", "dropbox");
