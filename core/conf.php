f7c96825748c0f59238057c4b6ac89473de09752
richard <richard@debian.(none)>
2009-07-15 21:02:19 -0700

    starting to migrate to defines in conf.php

--- core/conf.php
+++ core/conf.php
@@ -1,10 +1,17 @@
 <?php
 
 $loc = '';
-$path = '/var/vhosts/dropbox.easytospell.net';
+$path = '/var/vhosts/dropbox-dev.easytospell.net';
 $version = '0.3.0a';
 $url = 'dropbox-dev.easytospell.net';
 $secure = false; // use https for things that require passwords, upload, edit, delete
+
+// Start migrating to define based config
+define('DB_LOC', '');
+define('DB_PATH', '/var/vhosts/dropbox-dev.easytospell.net');
+define('DB_VERSION', '0.3.0a');
+define('DB_URL', 'dropbox-dev.easytospell.net');
+define('DB_SECURE', false);
 
 $db = new mysqli("localhost", "dropbox", "dropbox", "dropbox");
 if ( mysqli_connect_errno() ) {
