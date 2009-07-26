d10837750d2e0e76928c3ed0315cc0ad5f2146a8
Richard Marshall <richard@easytospell.net>
2009-07-25 18:24:41 -0700

    Fixed bug in tag search (cherry picked from commit 35306257a196d716544f606d4fac279bffe8317e)

--- core/pages/home.php
+++ core/pages/home.php
@@ -43,7 +43,7 @@ if ( $tags ) {
                 }
         }
         if ( $where ) 
-                $where = implode( ' && ', $where );
+                $where = ' && ' . implode( ' && ', $where );
 }
 
 if ( isset( $_SESSION['hide'] ) )
