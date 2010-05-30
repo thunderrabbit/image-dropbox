52ffd7ffa3ca3dbfb06fb71f33d1992035508d15
Richard Marshall <richard@easytospell.net>
2010-05-30 06:23:00 +0000

    Cleaned up core/pages/submit.php with new exception based image class. Moved most of the upload related logic into image class.

--- core/pages/submit.php
+++ core/pages/submit.php
@@ -2,31 +2,19 @@
 
 require DB_PATH . '/core/lib/image.php';
 
-$img = new image( $db );
+$img = new image($db);
 
-function error( $img, $rollback, $msg ) {
-	if ( $rollback ) {
+try {
+	$img->upload($authenticated);
+	$redirect = '/view/' . $img->getentryid();
+} catch (ImageException $e) {
+	if($e->rollback)
 		$img->rollback();
-	}
-	$_SESSION['upload_errors'] = $img->geterrors();
-	header('Location: http://' . DB_URL . DB_LOC . '/upload/');
-	exit();
-}
+	$_SESSION['upload_errors'][] = $e->getMessage();
+	$redirect = '/upload/';
+}	
 
-if ( !$img->checkpost( $authenticated ) ) error( $img, false, 'checkpost' );
-if ( !$img->checkduplicate() ) error( $img, false, 'checkduplicate' );
-$img->start();
-if ( !$img->createentry( $authenticated ) ) error( $img, true, 'createentry' );
-if ( !$img->createtags() ) error( $img, true, 'createtags' );
-if ( !$img->loadimage() ) error( $img, true, 'loadimage' );
-if ( !$img->createthumb() ) error( $img, true, 'createthumb' );
-if ( !$img->createthumb(1) ) error( $img, true, 'createthumb-custom' );
-if ( !$img->createdata() ) error( $img, true, 'createdata' );
-if ( !$img->createpreview() ) error( $img, true, 'createpreview' );
-$img->commit();
-
-header('Location: http://' . DB_URL . DB_LOC . '/view/' . $img->getentryid() );
-
+header('Location: http://' . DB_URL . DB_LOC . $redirect);
 $db->close();
 exit();
 
