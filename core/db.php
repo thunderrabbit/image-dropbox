e5ee3f83f2cddba69091540cf3e43bb7d11ab63e
Richard Marshall <richard@easytospell.net>
2010-05-30 01:59:20 +0000

    Changed from using standard mysqli class to dropbox_mysqli child class in preperation for memcached support.

--- core/db.php
+++ core/db.php
@@ -1,6 +1,8 @@
 <?php
 
-$db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_DB);
+require DB_PATH . '/core/lib/mysql.php';
+
+$db = new dropbox_mysqli(DB_HOST, DB_USER, DB_PASS, DB_DB);
 if(mysqli_connect_errno()) {
 	die("Could not connect to database");
 }
