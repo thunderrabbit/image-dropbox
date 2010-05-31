be867652c40833d798d48ea568a4862024731596
Rob Nugen <rob.git_dreamhost@robnugen.com>
2010-05-31 09:09:20 -0700

    fixed critical bug in dosignup (write hashed password to DB instead of plaintext) other fixes to signup and login

--- core/pages/dosignup.php
+++ core/pages/dosignup.php
@@ -1,6 +1,6 @@
 <?php
 
-print "signing user up...";
+debug("signing user up...");
 
 require DB_PATH . '/core/lib/validate.php';
 
@@ -8,34 +8,34 @@ if ( $authenticated ) die(" WTF? ");
 
 // check if username is "valid"
 if ( preg_match( '/^[a-zA-Z0-9]+$/', $_POST['username'] ) ) {
-	print "username valid";
+	debug("username valid");
 	$username = $db->safe( $_POST['username'] );
 	// apparently username is ok make sure it doesn't already exist
 	$sql = sprintf( "SELECT username FROM users WHERE username='%s'", $username );
 	if ( ! $db->exists( $sql ) ) {
-		print "username doesn't exist";
+		debug("username doesn't exist");
 		// looks like the username doesn't exist so lets go forth
 		// check if the alias contains anything weird
-		if ( preg_match( '/^[a-zA-Z0-9\-_.]+$/', $_POST['alias'] ) ) {
-			print "alias is valid";
+		if ( preg_match( '/^[a-zA-Z0-9\-_. ]+$/', $_POST['alias'] ) ) {
+			debug("alias is valid");
 			// apparently alias is ok
 			$alias = $db->safe( $_POST['alias'] );
 			
 			// make sure passwords match
 			if ( $_POST['password1'] == $_POST['password2'] ) {
-				print "passwords match";
+				debug("passwords match");
 				$password = $db->safe( $_POST['password1'] );
 				$salt = sha1( $username );
 				$password_hash = sha1( $password . $salt );	
 				
 				// make sure email is valid
 				if ( validate::email( $_POST['email'] ) ) {
-					print "email is valid";
+					debug("email is valid");
 					$email = $db->safe( $_POST['email']);
 					$email_hash = md5($email);
 					// everything seems ok lets make this user!
 					$sql = sprintf("INSERT INTO users (username,alias,email,email_hash,password,salt,joindate) VALUES ('%s','%s','%s','%s','%s','%s',UNIX_TIMESTAMP())",
-									$username,$alias,$email,$email_hash,$password,$salt);
+									$username,$alias,$email,$email_hash,$password_hash,$salt);
 					if ( ! $db->query( $sql ) ) die('failed query');
 					$id = $db->insert_id;
 
@@ -49,22 +49,22 @@ if ( preg_match( '/^[a-zA-Z0-9]+$/', $_POST['username'] ) ) {
 					$_SESSION['auth_email_hash'] = $email_hash;
 					setcookie('token',$auth_token,time()+DB_AUTH_TIMEOUT,'/',DB_URL,false,true);
 
-					header('Location: http://' . DB_URL . '/' . DB_LOC . 'me/' );
+					header('Location: http://' . DB_URL . DB_LOC . '/me/' );
 
 				} else {
-					print "error";
+					debug("error: invalid email");
 				}
 			} else {
-				print "error";
+				debug("error: passwords don't match");
 			}
 		} else {
-			print "error";
+			debug("error: invalid alias");
 		}
 	} else {
-		print "error";
+		debug("error: username exists");
 	}
 } else {
-	print "error";
+	debug("error: invalid username");
 }
 
 ?>
