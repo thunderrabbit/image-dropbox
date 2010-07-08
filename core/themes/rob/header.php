06b226ed6980e065475443ae0566061a79b4b4c7
Rob Nugen <rob.git_dreamhost@robnugen.com>
2010-07-07 19:55:16 -0700

    fixed rob/header to use rob/main.css

--- core/themes/rob/header.php
+++ core/themes/rob/header.php
@@ -2,7 +2,7 @@
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">	
 	<head>
-		<link rel="stylesheet" href="<?=DB_LOC;?>/media/theme/default/css/main.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="<?=DB_LOC;?>/media/theme/rob/css/main.css" type="text/css" media="screen" />
 		<title><?=DB_TITLE?></title>
 	</head>
 	<body>
