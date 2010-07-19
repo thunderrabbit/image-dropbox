f88e714ab292802cebe17e6e5c2d187b09aae44c
isFuzzy <thunderrabit+isfuzzy@gmail.com>
2010-07-19 06:10:47 -0700

    added fly2.nu specific info to themes/rob/about.php

--- core/themes/rob/about.php
+++ core/themes/rob/about.php
@@ -1,4 +1,15 @@
 <h2>About</h2>
+
+<div class="about">Part of the fly2.nu network, this site hosts images
+of its traveling stuffed animal, <?=DB_TITLE?>.  The software has been
+slightly modified from <a
+href="http://github.com/nils42/image-dropbox/">Nil's image-dropbox</a>
+on GitHub, and is available as <a
+href="http://github.com/thunderrabbit/image-dropbox/">thunderrabbit's
+image-dropbox</a> on GitHub.</div>
+
+<h2>About image-dropbox</h2>
+
 <div id="about">
 <p>Weekend project to implement an image storage system entirely in database form. The project has expanded well beyond the original few hours I put into it. At this point it is becoming a rather feature rich image hosting platform. Feel free to browse and use the source code at <a href="http://github.com/nils42/image-dropbox/">GitHub</a>. If you do use my code it would be nice if you let me know.</p>
 <p>Future features and current bugs viewable at:<br/> <a href="http://github.com/nils42/image-dropbox/issues/">http://github.com/nils42/image-dropbox/issues/</a></p>
